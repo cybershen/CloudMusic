71ed9db9a5232047382ebd3ae6587b519f81bf58
cybershen <cyberpuer@proton.me>
2022-12-24 13:35:33 +0200

    Initial Commit

---
+++ CloudMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CloudMusic
+//
+//  Created by Назар Жиленко on 24.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
