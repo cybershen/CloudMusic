18c4af396e705e6589d9933c71b6887fb9bc163d
cybershen <cyberpuer@proton.me>
2023-01-10 22:05:24 +0200

    Set up the Albums and Playlist api calls and ViewControllers

--- CloudMusic/Controllers/Core/HomeViewController.swift
+++ CloudMusic/Controllers/Core/HomeViewController.swift
@@ -15,6 +15,10 @@ enum BrowseSectionType {
 
 class HomeViewController: UIViewController {
     
+    private var newAlbums: [Album] = []
+    private var playlists: [Playlist] = []
+    private var tracks: [AudioTrack] = []
+    
     private var collectionView: UICollectionView = UICollectionView(
         frame: .zero,
         collectionViewLayout: UICollectionViewCompositionalLayout { sectionIndex, _ -> NSCollectionLayoutSection? in
@@ -154,7 +158,10 @@ class HomeViewController: UIViewController {
         newAlbums: [Album],
         playlists: [Playlist],
         tracks: [AudioTrack]) {
-            // Configure models
+            self.newAlbums = newAlbums
+            self.playlists = playlists
+            self.tracks = tracks
+            
             sections.append(.newReleases(viewModels: newAlbums.compactMap({
                 return NewReleasesCellViewModel(
                     name: $0.name,
@@ -174,7 +181,7 @@ class HomeViewController: UIViewController {
                 return RecommendedTrackCellViewModel(
                     name: $0.name,
                     artistName: $0.artists.first?.name ?? "Unknown",
-                    artworkURL: URL(string: $0.album.images.first?.url ?? ""))
+                    artworkURL: URL(string: $0.album?.images.first?.url ?? ""))
             })))
             collectionView.reloadData()
         }
@@ -240,6 +247,28 @@ extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSour
             let viewModel = viewModels[indexPath.row]
             cell.configure(with: viewModel)
             return cell
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        let section = sections[indexPath.section]
+        switch section {
+        case .featuredPlaylists:
+            let playlist = playlists[indexPath.row]
+            let vc = PlaylistViewController(playlist: playlist)
+            vc.title = playlist.name
+            vc.navigationItem.largeTitleDisplayMode = .never
+            navigationController?.pushViewController(vc, animated: true)
+            
+        case .newReleases:
+            let album = newAlbums[indexPath.row]
+            let vc = AlbumViewController(album: album)
+            vc.title = album.name
+            vc.navigationItem.largeTitleDisplayMode = .never
+            navigationController?.pushViewController(vc, animated: true)
+            
+        case .recommendedTracks: break
         }
     }
     
---
+++ CloudMusic/Models/PlaylistDetailsResponse.swift
@@ -0,0 +1,25 @@
+//
+//  PlaylistDetailsResponse.swift
+//  CloudMusic
+//
+//  Created by Назар Жиленко on 10.01.2023.
+//
+
+import Foundation
+
+struct PlaylistDetailsResponse: Codable {
+    let description: String
+    let external_urls: [String: String]
+    let id: String
+    let images: [APIImage]
+    let name: String
+    let tracks: PlaylistTracksResponse
+}
+
+struct PlaylistTracksResponse: Codable {
+    let items: [PlaylistItem]
+}
+
+struct PlaylistItem: Codable {
+    let track: AudioTrack
+}
