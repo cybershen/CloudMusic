bd2423d13a63a7ed09d3af282fba93d382d044d5
cybershen <cyberpuer@proton.me>
2023-03-08 21:39:41 +0200

    Add search categories for SearchViewController. Fetch data and display it

---
+++ CloudMusic/ViewModels/CategoryCollectionViewCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryCollectionViewCellViewModel.swift
+//  CloudMusic
+//
+//  Created by Назар Жиленко on 08.03.2023.
+//
+
+import Foundation
+
+struct CategoryCollectionViewCellViewModel {
+    let title: String
+    let artworkURL: URL?
+}
